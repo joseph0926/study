90239c7564a087be5a45d5ee56548c430b3912c9
김영훈 <joseph0926@naver.com>
2024-06-25 13:46:54 +0900

    vue: [1] supabase

---
+++ vue/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import { supabase } from './lib/supabaseClient'
+</script>
+
+<template>
+  <main>
+    <RouterView />
+  </main>
+</template>
