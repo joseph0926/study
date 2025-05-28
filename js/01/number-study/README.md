a65914074c17c82bc6cd5ef268462cda706e420c
김영훈 <joseph0926@naver.com>
2025-05-28 11:02:21 +0900

    study: 01.1 number

--- js/01/number-study/README.md
+++ js/01/number-study/README.md
@@ -4,13 +4,22 @@
 
 - Number.MAX_SAFE_INTEGER + 1 === Number.MAX_SAFE_INTEGER + 2 가 true 인 이유는?
 
+  - Number.MAX_SAFE_INTEGER가 53비트 단위일것이고 여기에 +1을 하나 +2를 하나 53비트를 초과하여 반올림되어 같은 값으로 근사됨
+
 - parseInt('08') === 8 이 항상 보장될까?
+
+  - 08이 8진수로 판단 될 수 있으므로 안정성을 위해 `parseInt("08", 10)`으로 정확히 명시해야함
 
 - Math.round((0.105+Number.EPSILON)\*100)/100 의 결과는?
 
+  - Number.EPSILON??? -> Number.EPSILON은 2⁻⁵² ≈ 2.22 × 10⁻¹⁶ — JS가 표현할 수 있는 가장 작은 차이
+
 - Infinity - Infinity 의 값은?
 
+  - NaN
+
 - JSON 직렬화 시 BigInt 를 포함한 객체를 stringify 하면 어떻게 될까?
+  - JSON.stringify는 BigInt를 지원하지 않음 -> 따라서 BigInt를 string으로 변환한 후 JSON으로 변환해야함
 
 ### EX
 
--- js/01/number-study/src/fix-float.ts
+++ js/01/number-study/src/fix-float.ts
@@ -9,11 +9,11 @@ export function fixFloatConsole() {
 
 /**
  - 0.1 + 0.2 가 0.300000… 으로 끝나는 이유는?
-    - 0.xx는 정확히 그 숫자가 아니라서...?
+    - js의 Number는 64 bit 부동소수점인데, js에서 실제로 값을 표현하는 부분은 53 유효비트뿐이라 0.1·0.2 같은 무한소수를 정확히 담지못하여 근사값인 0.30000000000000004가 나옴
  - +0, -0 를 굳이 구분해야 할까요? 언제 유용할까요?
-   - 모르겠음
+   - 부호는 방향정보를 뜻할 수 있음 1/+0 = Infinity, 1/-0 = -Infinity
  - NaN === NaN 이 false 인데, “NaN 검사” 를 안전하게 하려면?
-   - Number.isNaN()
+   - Number.isNaN(x) or Object.is(x, NaN)
  */
 
 /**
@@ -24,6 +24,9 @@ export function fixFloatConsole() {
  * 과제 : fixFloat 함수를 완성해 “소수점 오차” 를 지정한 자릿수(d) 까지 반올림합니다.
  * ex) fixFloat(0.1 + 0.2) === 0.3
  */
-export const fixFloat = (n: number, d = 12): number => {
-  return +n.toFixed(d);
-};
+// export const fixFloat = (n: number, d = 12): number => {
+//   return +n.toFixed(d);
+// };
+
+export const fixFloat = (n: number, d = 12) =>
+  Math.round((n + Number.EPSILON) * 10 ** d) / 10 ** d;
